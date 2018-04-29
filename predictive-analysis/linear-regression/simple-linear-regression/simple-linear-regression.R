ee5ff05c337d8bde7d8c607268dff576eba919a2
Prem Pranav <38777813+s-kayashyap@users.noreply.github.com>
2018-04-29 18:58:50 +0530

    Simple Linear Regression Model
    R code to create simple linear regression model for TV marketing company.

---
+++ predictive-analysis/linear-regression/simple-linear-regression/simple-linear-regression.R
@@ -0,0 +1,45 @@
+# Simple Linear Regression modeling 
+# Input Dataset : tvmarketing.csv
+# Name : Swami Prem Pranav Kayashyap (APFE1786831)
+# ------------------------------------------------------
+# Date : 21st Apr, 2018
+
+library(ggplot2)
+#Import advertising data from tvmarketing.csv 
+advertising <- read.csv(file.choose(),stringsAsFactors = F)
+
+str(advertising)
+
+# Scatter Plot (TV vs Sales)
+ggplot(advertising, aes(TV, Sales)) +
+  geom_point(aes(colour = "red" )) +
+  geom_smooth(method=lm) +
+  scale_x_continuous(name = "Marketing budget(TV)") + 
+  scale_y_continuous(name = "Sales")
+
+#The set.seed( ) command is used to reproduce the same results each time while sampling 70%
+#data for the training dataset. This is done so that you get the same training data as your sample
+#each time you execute the R code
+set.seed(100)  
+
+# Use sample function for getting the random number represents 70% of indices. 
+training_indexes = sample(1:nrow(advertising), 0.7*nrow(advertising))
+
+#Create training data objects and test data objects
+training_data <- advertising[training_indexes,]
+test_data <- advertising[-training_indexes,]
+
+#Create a simple linear regression model
+SLR_model<-lm(Sales~TV, data = training_data)
+
+## Check the summary of model. 
+summary(SLR_model)
+
+#Predict the sales price of test data using this SLR_model and add this result with test_data
+# to evaluate our model's efficiency
+test_data$predicted_sales <- predict(SLR_model, test_data[,-2])
+
+#One fact is that , R_Square value of the model is equivalent to square of correlation between
+#actual output and predicted output from model. Just verify.
+R <- cor(test_data$Sales,test_data$predicted_sales)
+R_squared <- R^2
